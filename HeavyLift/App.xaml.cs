497eb200b5b84639b8d4aaf68ed653d3843542d9
Dawid Mackiewicz <dawid2004m@gmail.com>
2025-05-02 02:27:27 +0200

    implemtended styles needed for login view, collor pallet and lgoin panel UI

--- HeavyLift/App.xaml.cs
+++ HeavyLift/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace HeavyLift
+﻿using HeavyLift.Views;
+
+namespace HeavyLift
 {
     public partial class App : Application
     {
@@ -9,7 +11,8 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            //return new Window(new AppShell());
+            return new Window(new LoginView());
         }
     }
 }
