14b720b37d44b406f1a58f5680708e1eb8982958
Dawid Mackiewicz <dawid2004m@gmail.com>
2025-05-03 19:18:38 +0200

    set up of LoginViewModel

--- HeavyLift/ViewModels/LoginViewModel.cs
+++ HeavyLift/ViewModels/LoginViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,53 @@ using System.Threading.Tasks;
 
 namespace HeavyLift.ViewModels
 {
-    internal class LoginViewModel: ObservableObject
+    internal partial class LoginViewModel : ObservableObject
     {
+        [ObservableProperty]
+        public string _emailInput;
+        [ObservableProperty]
+        public string _passwordInput;
+        [ObservableProperty]
+        public string _errorMessage;
 
+        public string Tekscior { get; set; }
+
+        [RelayCommand]
+        public async Task Login()
+        {
+            ErrorMessage = "";
+            if (!IsValidEmail(EmailInput))
+            {
+                ErrorMessage += "Invalid email format.\n";
+            }
+            if(string.IsNullOrWhiteSpace(PasswordInput))
+            {
+                ErrorMessage = "Please fill in all fields.\n";
+                return;
+            }
+        }
+        [RelayCommand]
+        public void SwitchToRegisterPage()
+        {
+            
+        }
+
+        public LoginViewModel()
+        {
+            ErrorMessage = "";
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            return System.Text.RegularExpressions.Regex.IsMatch(
+                email,
+                @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+        }
     }
 }
